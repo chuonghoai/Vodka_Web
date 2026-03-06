72f8d9161568bce12c89c795a91060bbe84b9cf2
HongHac <144915081+chuonghoai@users.noreply.github.com>
2026-03-06 17:38:23 +0700

    build pattern

---
+++ src/app/features/home/components/movie-carousel/movie-carousel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-carousel',
+  imports: [],
+  templateUrl: './movie-carousel.html',
+  styleUrl: './movie-carousel.css',
+})
+export class MovieCarousel {}
---
+++ src/app/features/home/components/top-ranking/top-ranking.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-ranking',
+  imports: [],
+  templateUrl: './top-ranking.html',
+  styleUrl: './top-ranking.css',
+})
+export class TopRanking {}
